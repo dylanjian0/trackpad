056e548cb77d35d8aba58c7b6b07ca7e299a348e
Dylan Jian <dylanjian0@gmail.com>
2025-04-09 19:27:22 -0400

    Subject model

--- backend/api/models.py
+++ backend/api/models.py
@@ -6,3 +6,9 @@ class Instructor(models.Model):
     def __str__(self):
         return self.name
 
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+

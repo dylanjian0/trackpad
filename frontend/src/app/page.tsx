ea51bddd456cb49a379e66ff71c841482b99acfb
Dylan Jian <dylanjian0@gmail.com>
2025-04-09 12:03:39 -0400

    django project setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <div className="min-h-screen">
       <Navbar/>
+      <h1>Trackpad</h1>
       {/* <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
           <Image
